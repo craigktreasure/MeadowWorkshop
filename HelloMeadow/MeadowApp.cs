0847c4e6df86a008c5d36823f64019be8c1bc9da
Craig Treasure <craigktreasure@outlook.com>
2019-12-14 15:47:22 -0800

    Use Meadow.Foundation.Leds.RgbLed

--- HelloMeadow/MeadowApp.cs
+++ HelloMeadow/MeadowApp.cs
@@ -2,6 +2,7 @@
 {
     using Meadow;
     using Meadow.Devices;
+    using Meadow.Foundation.Leds;
     using Meadow.Library;
     using Meadow.Library.Peripherals;
     using System;
@@ -11,7 +12,8 @@
     {
         public MeadowApp()
         {
-            this.CycleLeds();
+            //this.CycleLeds();
+            this.CycleLedsMeadowFoundation();
         }
 
         public void CycleLeds()
@@ -25,6 +27,31 @@
             while (true)
             {
                 currentColor = currentColor.GetNext();
+                Console.WriteLine($"Setting color to {currentColor}.");
+                led.SetColor(currentColor);
+                Thread.Sleep(wait);
+            }
+        }
+
+        private void CycleLedsMeadowFoundation()
+        {
+            const int wait = 200;
+
+            RgbLed led = new RgbLed(Device,
+                redPin: Device.Pins.OnboardLedRed,
+                greenPin: Device.Pins.OnboardLedGreen,
+                bluePin: Device.Pins.OnboardLedBlue);
+
+            RgbLed.Colors currentColor = RgbLed.Colors.count;
+
+            while (true)
+            {
+                do
+                {
+                    currentColor = currentColor.GetNext();
+                }
+                while (currentColor == RgbLed.Colors.count); // Skip RgbLed.Colors.count
+
                 Console.WriteLine($"Setting color to {currentColor}.");
                 led.SetColor(currentColor);
                 Thread.Sleep(wait);
